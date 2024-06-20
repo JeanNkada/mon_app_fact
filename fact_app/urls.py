45c251c24d35d09fd75f4abcd25d69774d8ce5c0
Jean Nkada <nkadajohn1@gmail.com>
2024-06-20 14:59:02 +0100

    Enregistrement d'un nouveau client

--- fact_app/urls.py
+++ fact_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('add_customer', views.AddCustomerView.as_view(), name='add-customer')
     
 ]
 
--- fact_app/views.py
+++ fact_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,19 +10,52 @@ class HomeView(View):
     
     templates_name = 'index.html'
     
-    """on utilise select_related par ce que on a des champs many to many cela 
+    """on utilise select_related par ce qu'on a des champs many to many cela 
     va facilité les requêtes.
     en fait ça fait ce qu'on appelle le sql any
-    le any ça prend plusieurs tables reliés à une table 
-    et le join pour en faire une seul table """
+    le any prend plusieurs tables reliés à une table 
+    et les joins pour en faire une seul table """
     
     invoices = Invoice.objects.select_related('customer', 'save_by').all()
     context = {
         'invoices': invoices
     }
-    def get (self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         
         return render(request, self.templates_name, self.context)
     
-    def post (self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         return render(request, self.templates_name, self.context)
+    
+# dedut du coded'enregistrement d'un nouveau client
+class AddCustomerView(View):
+    """Add new customer"""
+    templates_name = 'add_customer.html'
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.templates_name)
+
+    def post(self, request, *args, **kwargs):
+        data ={
+            'name': request.POST.get('name'),
+            'email': request.POST.get('email'),
+            'phone': request.POST.get('phone'),
+            'address': request.POST.get('address'),
+            'age': request.POST.get('age'),
+            'city': request.POST.get('city'),
+            'sex': request.POST.get('sex'),
+            'zip_code': request.POST.get('zip_code'),
+            'save_by': request.user
+                }
+        try:
+            created = Customer.objects.create(**data)
+            if created:
+                messages.success(request, 'Customer registered successfully.')
+            else:
+                messages.error(request, 'Sorry, pleace try again the sent data is corrupt.')
+                
+        except Exception as e:
+            messages.error(request, f'Sorry our system is detecting the following issues {e}')
+            
+        return render(request, self.templates_name)
+# fin d'enrégistrement du nouveau client
