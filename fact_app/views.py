e383a537bd842956b81ba8e12530eaa1ccf23658
Jean Nkada <nkadajohn1@gmail.com>
2024-06-20 09:42:18 +0100

    lister les facture

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
 # Create your views here.
 
-def home(request, *args, **kwargs):
-    return render(request, "base.html", {})
+class HomeView(View):
+    """Main View"""
+    
+    templates_name = 'index.html'
+    
+    """on utilise select_related par ce que on a des champs many to many cela 
+    va facilité les requêtes.
+    en fait ça fait ce qu'on appelle le sql any
+    le any ça prend plusieurs tables reliés à une table 
+    et le join pour en faire une seul table """
+    
+    invoices = Invoice.objects.select_related('customer', 'save_by').all()
+    context = {
+        'invoices': invoices
+    }
+    def get (self, request, *args, **kwargs):
+        
+        return render(request, self.templates_name, self.context)
+    
+    def post (self, request, *args, **kwargs):
+        return render(request, self.templates_name, self.context)
